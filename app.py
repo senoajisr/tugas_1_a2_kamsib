1e85d9f6aaf71a4739e959fb8e8c0efe425c7b71
senoajisr <xsenoajix@gmail.com>
2025-12-04 16:27:02 +0700

    add: validate grade string is below its character limit

--- app.py
+++ app.py
@@ -83,6 +83,8 @@ def edit_student(id) -> Response|str:
 def verify_form(name: str, age: str, grade: str) -> bool:
     if not verify_age_form(age):
         return False
+    if not validate_characters_is_below_limit(grade, STUDENT_GRADE_CHARACTER_LIMIT):
+        return False
     return True
 
 
